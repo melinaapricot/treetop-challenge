9bb49662360b16ff49a38f555e60bea834e8a24a
Melina <melinaverikios@gmail.com>
2022-11-28 17:11:20 +0100

    create basic component picker

--- treetop-challenge/src/App.vue
+++ treetop-challenge/src/App.vue
@@ -1,15 +1,95 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template class="picker">
+  <h1> Welcome To Treetop Component Picker </h1>
+<div class="picker__lists">
+  <ul>
+    <li class="picker__item">
+      <TreetopComponent
+      class="picker__component"
+        v-for="component in components"
+        :key="component.code"
+        :name="component.name"
+        :code="component.code"
+        :details="component.details">
+          <input type="checkbox" v-model="componentList" :value="component.name" class="picker__checkbox">
+      </TreetopComponent>
+    </li>
+  </ul>
+  <div>
+    <h2>Selected: </h2>
+    <ul>
+       <li v-for="(selectedComponent, i) in componentList" :key="selectedComponent"> {{selectedComponent}}
+         <button type="button" @click="deleteItem(i)"> remove </button>
+        </li>
+    </ul>
+  </div>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TreetopComponent from './components/TreetopComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TreetopComponent,
+  },
+  data() {
+    return {
+      componentList: [],
+      components: [{
+            name: 'Component A',
+            code: 20430,
+            details: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. '
+          },
+          {
+            name: 'Component B',
+            code: 83499,
+            details: 'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+          },
+          {
+            name: 'Component C',
+            code: 50895,
+            details: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.'
+          },
+          {
+            name: 'Component D',
+            code: 90098,
+            details: 'sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.'
+          },
+          {
+            name: 'Component E',
+            code: 93243,
+            details: 'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+          },
+          {
+            name: 'Component F',
+            code: 87232,
+            details: 'Lorem Ipsum ajnfafnakjfnödkjsnfdökjf',
+          },
+          {
+            name: 'Component G',
+            code: 98348,
+            details: 'Lorem Ipsum ajnfafnakjfnödkjsnfdökjf',
+          },
+          {
+            name: 'Component H',
+            code: 93474,
+            details: 'Lorem Ipsum ajnfafnakjfnödkjsnfdökjf',
+          },
+          {
+            name: 'Component I',
+            code: 45368,
+            details: 'Lorem Ipsum ajnfafnakjfnödkjsnfdökjf',
+          },
+        ],
+    }
+  },
+  methods: {
+    deleteItem(i) {
+      this.componentList.splice(i, 1);
+    }
   }
 }
 </script>
@@ -17,10 +97,20 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+.picker__lists {
+  display: flex;
+}
+
+.picker__item,
+.picker__component {
+  list-style: none;
+}
+
+.picker__checkbox {
+  margin-left: 2rem;
+}
+
 </style>
---
+++ treetop-challenge/src/components/TreetopComponent.vue
@@ -0,0 +1,81 @@
+<template>
+  <div :class="{ 'component': true, 'component--open': showDetails } ">
+    <div class="component__content">
+        <label class="component__label">
+          <h3 class="component__name"> {{ name }} </h3>
+          <slot/>
+        </label>
+        <span class="component__code"> Code: {{ code }} </span>
+        <button type="button" class="component__detail-btn" @click.prevent="toggleDetails()"> {{showDetails === false? 'Show Details' : 'Hide Details'}} </button>
+        <p v-if="showDetails" class="component__details"> {{ details }} </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TreetopComponent',
+  props: {
+    name: String,
+    code: Number,
+    details: String,
+  },
+  data() {
+    return {
+      showDetails: false,
+      selectedComponents: [
+      ],
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    toggleDetails() {
+      return this.showDetails = !this.showDetails;
+    },
+
+  },
+}
+</script>
+<style scoped lang="scss">
+
+h3 {
+  margin: 40px 0 0;
+}
+
+.component {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 20rem;
+  height: 7rem;
+  margin-bottom: 1rem;
+  text-align: left;
+  &--open {
+    height: unset;
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__label {
+    display: flex;
+    align-items: baseline;
+  }
+
+  &__code {
+    margin-top: 0.5rem;
+  }
+
+  &__detail-btn {
+    margin-top: 0.5rem;
+    width: 6rem;
+  }
+
+  &__details {
+  }
+}
+</style>
